6e956141fd2f6fc20578ca9cde511747ec101623
three-kings-code <64013976+three-kings-code@users.noreply.github.com>
2023-10-10 15:04:23 +0100

    #29 created cdn and serve s3 bucket from cdn (#30)

--- output.tf
+++ output.tf
@@ -7,3 +7,8 @@ output "website_endpoint_output" {
   description = "S3 Static website URL"
   value = module.terrahouse_aws.website_endpoint
 }
+
+output "cloudfront_url" {
+  description = "This will be the url that we need to use to access the website"
+  value = module.terrahouse_aws.cloudfront_url
+}
