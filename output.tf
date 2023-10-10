0541f5814e0abb88512af7c54b302508be691a87
three-kings-code <johnson.king@btinternet.com>
2023-10-10 08:57:38 +0000

    #27 we enabled static website hosting and we also uploaded html files via terraform

--- output.tf
+++ output.tf
@@ -1,3 +1,9 @@
 output "bucket_name_output" {
-  value = module.terrahouse_aws.bucket_name_output
+  description = "S3 storage bucket for static website"
+  value = module.terrahouse_aws.bucket_name
+}
+
+output "website_endpoint_output" {
+  description = "S3 Static website URL"
+  value = module.terrahouse_aws.website_endpoint
 }
