4e814c137c22d54b97796536a1f42b242f4cee43
three-kings-code <johnson.king@btinternet.com>
2023-10-12 14:01:18 +0000

    #41 should be able to create run terraform provider block without errors

--- output.tf
+++ output.tf
@@ -1,14 +1,14 @@
-output "bucket_name_output" {
-  description = "S3 storage bucket for static website"
-  value = module.terrahouse_aws.bucket_name
-}
+# output "bucket_name_output" {
+#   description = "S3 storage bucket for static website"
+#   value = module.terrahouse_aws.bucket_name
+# }
 
-output "website_endpoint_output" {
-  description = "S3 Static website URL"
-  value = module.terrahouse_aws.website_endpoint
-}
+# output "website_endpoint_output" {
+#   description = "S3 Static website URL"
+#   value = module.terrahouse_aws.website_endpoint
+# }
 
-output "cloudfront_url" {
-  description = "This will be the url that we need to use to access the website"
-  value = module.terrahouse_aws.cloudfront_url
-}
+# output "cloudfront_url" {
+#   description = "This will be the url that we need to use to access the website"
+#   value = module.terrahouse_aws.cloudfront_url
+# }
