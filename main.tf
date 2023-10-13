3df694862cf890f4cc2de48c01df530ba44ac584
three-kings-code <64013976+three-kings-code@users.noreply.github.com>
2023-10-13 14:29:18 +0100

    #46 tested our terra home in terratowns test town and amended html pages to get them to work (#47)

--- main.tf
+++ main.tf
@@ -6,10 +6,6 @@ terraform {
 #     }
 #   }
   required_providers {
-    # aws = {
-    #   source = "hashicorp/aws"
-    #   version = "5.19.0"
-    #  }
     terratowns = {
       source = "local.providers/local/terratowns"
       version = "1.0.0"
@@ -18,32 +14,36 @@ terraform {
 
 }
 
-# provider "aws" {
-#   # Configuration options
-# }
-
 provider "terratowns" {
-  endpoint = "http://localhost:4567/api"
-  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
-  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+  endpoint = var.terratowns_endpoint
+  user_uuid = var.teacherseat_user_uuid 
+  token = var.terratowns_access_token
 }
 
-
-# module "terrahouse_aws" {
-#     source = "./modules/terrahouse_aws"
-#     user_uuid = var.user_uuid
-#     bucket_name = var.bucket_name
-#     index_html_filepath = var.index_html_filepath
-#     error_html_filepath = var.error_html_filepath
-#     content_version = var.content_version
-#     assets_path = var.assets_path
+# #mock town
+# provider "terratowns" {
+#   endpoint = "http://localhost:4567/api"
+#   user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
+#   token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
 # }
+
+
+module "terrahouse_aws" {
+    source = "./modules/terrahouse_aws"
+    user_uuid = var.teacherseat_user_uuid
+    bucket_name = var.bucket_name
+    index_html_filepath = var.index_html_filepath
+    error_html_filepath = var.error_html_filepath
+    content_version = var.content_version
+    assets_path = var.assets_path
+    htmls_filepath = var.htmls_filepath
+}
 
 resource "terratowns_home" "Dizzy" {
   name = "Dizzy the Egg Fan Site"
-  # domain_name = module.terrahouse_aws.cloudfront_url
-  domain_name = "34asda32.cloudfront.net"
-  description = "This site is a fan site which talks about the game where the main character is Dizzy and he has friends that help him along the way"
-  town = "gamers-grotto"
+  domain_name = module.terrahouse_aws.cloudfront_url
+  #domain_name = "34asda32.cloudfront.net"
+  description = "This site is a fan site which talks about the game where the main character is Dizzy"# and he has friends that help him along the way"
+  town = "missingo"
   content_version = 1
 }
